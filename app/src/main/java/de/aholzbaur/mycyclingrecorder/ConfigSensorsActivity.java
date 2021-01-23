d9d09a4b1b2e8470777d9df2c42a190a8de62107
aholzbaur <71641117+aholzbaur@users.noreply.github.com>
2021-01-23 22:21:27 +0100

    Show list of paired devices

--- app/src/main/java/de/aholzbaur/mycyclingrecorder/ConfigSensorsActivity.java
+++ app/src/main/java/de/aholzbaur/mycyclingrecorder/ConfigSensorsActivity.java
@@ -1,6 +1,7 @@
 package de.aholzbaur.mycyclingrecorder;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,17 +9,21 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Set;
+
 public class ConfigSensorsActivity extends AppCompatActivity {
     private TextView textBtDisabledHint = null;
-    private TextView textDeviceNameTitle = null;
-    private TextView textDeviceAsSensor = null;
+    private TextView textBtDeviceNameTitle = null;
+    private TextView textBtDeviceAsSensor = null;
 
-    private ScrollView scrollAvailDeviceList = null;
+    private ScrollView scrollAvailBtDeviceList = null;
+    private LinearLayout scrollAvailBtDeviceListLayout = null;
 
     private Button buttonEnableBt = null;
 
@@ -42,10 +47,11 @@ public class ConfigSensorsActivity extends AppCompatActivity {
 
     private void getItems() {
         this.textBtDisabledHint = this.findViewById(R.id.textBtDisabledHint);
-        this.textDeviceNameTitle = this.findViewById(R.id.textDeviceNameTitle);
-        this.textDeviceAsSensor = this.findViewById(R.id.textDeviceAsSensor);
+        this.textBtDeviceNameTitle = this.findViewById(R.id.textBtDeviceNameTitle);
+        this.textBtDeviceAsSensor = this.findViewById(R.id.textBtDeviceAsSensor);
 
-        this.scrollAvailDeviceList = this.findViewById(R.id.scrollAvailDeviceList);
+        this.scrollAvailBtDeviceList = this.findViewById(R.id.scrollAvailBtDeviceList);
+        this.scrollAvailBtDeviceListLayout = this.findViewById(R.id.scrollAvailBtDeviceListLayout);
 
         this.buttonEnableBt = this.findViewById(R.id.buttonEnableBt);
         this.buttonEnableBt.setOnClickListener(new View.OnClickListener() {
@@ -62,15 +68,38 @@ public class ConfigSensorsActivity extends AppCompatActivity {
         if (this.bluetoothAdapter.isEnabled() == false) {
             this.textBtDisabledHint.setVisibility(View.VISIBLE);
             this.buttonEnableBt.setVisibility(View.VISIBLE);
-            this.textDeviceNameTitle.setVisibility(View.GONE);
-            this.textDeviceAsSensor.setVisibility(View.GONE);
-            this.scrollAvailDeviceList.setVisibility(View.GONE);
+            this.textBtDeviceNameTitle.setVisibility(View.GONE);
+            this.textBtDeviceAsSensor.setVisibility(View.GONE);
+            this.scrollAvailBtDeviceList.setVisibility(View.GONE);
         } else {
             this.textBtDisabledHint.setVisibility(View.GONE);
             this.buttonEnableBt.setVisibility(View.GONE);
-            this.textDeviceNameTitle.setVisibility(View.VISIBLE);
-            this.textDeviceAsSensor.setVisibility(View.VISIBLE);
-            this.scrollAvailDeviceList.setVisibility(View.VISIBLE);
+            this.textBtDeviceNameTitle.setVisibility(View.VISIBLE);
+            this.textBtDeviceAsSensor.setVisibility(View.VISIBLE);
+            this.scrollAvailBtDeviceList.setVisibility(View.VISIBLE);
+
+            fillAvailBtDevicesList();
+        }
+    }
+
+    void fillAvailBtDevicesList() {
+        this.scrollAvailBtDeviceListLayout.removeAllViews();
+
+        Set<BluetoothDevice> availBtDevices = this.bluetoothAdapter.getBondedDevices();
+
+        if (availBtDevices.size() > 0) {
+            for (BluetoothDevice device : availBtDevices) {
+                LinearLayout newListItem = (LinearLayout) this.getLayoutInflater().inflate(R.layout.avail_bt_device_list_item, null);
+                TextView newListItemName = (TextView) newListItem.getChildAt(0);
+
+                newListItemName.setText(device.getName());
+
+                this.scrollAvailBtDeviceListLayout.addView(newListItem);
+            }
+        } else {
+            LinearLayout emptyListItem = (LinearLayout) this.getLayoutInflater().inflate(R.layout.avail_bt_device_list_item, null);
+
+            this.scrollAvailBtDeviceListLayout.addView(emptyListItem);
         }
     }
 
@@ -85,11 +114,13 @@ public class ConfigSensorsActivity extends AppCompatActivity {
                 }
             }
         };
+
         this.registerReceiver(this.btStateReceiver, new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
     }
 
     private void showEnableBtIntent() {
         Intent enableBluetoothIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+        
         this.startActivity(enableBluetoothIntent);
     }
 }
